c2836f195b4566a4c2bf250394cd6a6c255fb74a
Ant <130005196+LoQiseaking69@users.noreply.github.com>
2024-07-07 08:58:44 -0400

    Update uart_comm.v

--- fpga/uart_comm.v
+++ fpga/uart_comm.v
@@ -1,1 +1,12 @@
+module uart_comm(
+    input wire clk,
+    input wire reset,
+    input wire [31:0] data_in,
+    output wire [31:0] data_out,
+    input wire uart_rx,
+    output wire uart_tx
+);
 
+// UART communication implementation
+
+endmodule
