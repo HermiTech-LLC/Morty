f1c3ba9a1c601eed9443318f2b3f4d69264c58d9
Ant <130005196+LoQiseaking69@users.noreply.github.com>
2024-07-10 23:46:00 -0400

    Update mortymb.py

--- mother/mortymb.py
+++ mother/mortymb.py
@@ -8,15 +8,17 @@ pcie_gnd = Net('PCIE_GND')
 usb_vcc = Net('USB_VCC')
 eth_vcc = Net('ETH_VCC')
 sata_vcc = Net('SATA_VCC')
+fpga_vcc = Net('FPGA_VCC')
+fpga_gnd = Net('FPGA_GND')
 
-# Define the CPU
-cpu = Part('device', 'U', value='High-Performance CPU', footprint='QFP-64')
+# Define the CPU (Example: AMD Ryzen 7)
+cpu = Part('amd', 'Ryzen7', footprint='AM4')
 vcc += cpu['VCC']
 gnd += cpu['GND']
 
 # Define RAM (e.g., DDR4 modules)
-ram1 = Part('device', 'RAM', value='DDR4', footprint='DIMM-288')
-ram2 = Part('device', 'RAM', value='DDR4', footprint='DIMM-288')
+ram1 = Part('memory', 'DDR4', footprint='DIMM-288')
+ram2 = Part('memory', 'DDR4', footprint='DIMM-288')
 vcc += ram1['VCC'], ram2['VCC']
 gnd += ram1['GND'], ram2['GND']
 cpu['ADDR0', 'ADDR1', 'ADDR2', 'ADDR3', 'ADDR4', 'ADDR5', 'ADDR6', 'ADDR7'] += ram1['ADDR0', 'ADDR1', 'ADDR2', 'ADDR3', 'ADDR4', 'ADDR5', 'ADDR6', 'ADDR7']
@@ -24,18 +26,27 @@ cpu['DATA0', 'DATA1', 'DATA2', 'DATA3', 'DATA4', 'DATA5', 'DATA6', 'DATA7'] += r
 cpu['ADDR0', 'ADDR1', 'ADDR2', 'ADDR3', 'ADDR4', 'ADDR5', 'ADDR6', 'ADDR7'] += ram2['ADDR0', 'ADDR1', 'ADDR2', 'ADDR3', 'ADDR4', 'ADDR5', 'ADDR6', 'ADDR7']
 cpu['DATA0', 'DATA1', 'DATA2', 'DATA3', 'DATA4', 'DATA5', 'DATA6', 'DATA7'] += ram2['DATA0', 'DATA1', 'DATA2', 'DATA3', 'DATA4', 'DATA5', 'DATA6', 'DATA7']
 
-# Define PCIe slots for GPUs
+# Define FPGA (Example: Xilinx Spartan-6)
+fpga = Part('xilinx', 'Spartan6', footprint='BGA-256')
+fpga_vcc += fpga['VCC']
+fpga_gnd += fpga['GND']
+
+# UART communication between CPU and FPGA
+cpu['UART_TX'] += fpga['UART_RX']
+fpga['UART_TX'] += cpu['UART_RX']
+
+# Define PCIe slots for GPUs (Example: PCIe x16)
 pcie_slots = []
 for i in range(6):
-    pcie_slot = Part('connector', 'PCIE_SLOT', value=f'PCIe Slot {i+1}', footprint='PCIEX16')
+    pcie_slot = Part('connector', 'PCIE_SLOT', footprint='PCIEX16')
     pcie_vcc += pcie_slot['VCC']
     pcie_gnd += pcie_slot['GND']
     pcie_slots.append(pcie_slot)
 
-# Define 6 GPUs
+# Define GPUs (Example: AMD Radeon RX)
 gpus = []
 for i in range(6):
-    gpu = Part('device', 'GPU', value=f'GPU {i+1}', footprint='BGA-256')
+    gpu = Part('amd', 'RadeonRX', footprint='BGA-256')
     vcc += gpu['VCC']
     gnd += gpu['GND']
     pcie_vcc += gpu['PCIE_VCC']
@@ -49,41 +60,41 @@ for i in range(6):
     pcie_slots[i]['TX'] += gpus[i]['PCIE_TX']
     gpus[i]['PCIE_RX'] += pcie_slots[i]['RX']
 
-# Define power management IC
-pmic = Part('device', 'PMIC', value='Power Management IC', footprint='QFN-32')
+# Define power management IC (Example: TI TPS65217)
+pmic = Part('ti', 'TPS65217', footprint='QFN-32')
 vcc += pmic['VCC']
 gnd += pmic['GND']
 pmic['OUT1'] += cpu['VCC']
 pmic['OUT2'] += [ram1['VCC'], ram2['VCC'], pcie_vcc]
 
-# Define motherboard connectors
-atx_power = Part('connector', 'ATX_POWER', value='ATX Power Connector', footprint='ATX-24')
+# Define motherboard connectors (Example: ATX)
+atx_power = Part('connector', 'ATX_POWER', footprint='ATX-24')
 vcc += atx_power['VCC']
 gnd += atx_power['GND']
 atx_power['12V'] += pmic['IN']
 
-# Define USB Controller and Ports
-usb_ctrl = Part('device', 'USB_CONTROLLER', value='USB 3.0 Controller', footprint='QFN-64')
+# Define USB Controller and Ports (Example: NEC D720200)
+usb_ctrl = Part('nec', 'D720200', footprint='QFN-64')
 vcc += usb_ctrl['VCC']
 gnd += usb_ctrl['GND']
 cpu['USB_TX', 'USB_RX'] += usb_ctrl['TX', 'RX']
 
 usb_ports = []
 for i in range(4):
-    usb_port = Part('connector', 'USB_PORT', value=f'USB Port {i+1}', footprint='USB-A')
+    usb_port = Part('connector', 'USB_PORT', footprint='USB-A')
     usb_vcc += usb_port['VCC']
     gnd += usb_port['GND']
     usb_port['D+'] += usb_ctrl[f'D+{i}']
     usb_port['D-'] += usb_ctrl[f'D-{i}']
     usb_ports.append(usb_port)
 
-# Define Ethernet Controller and Ports
-eth_ctrl = Part('device', 'ETHERNET_CONTROLLER', value='Gigabit Ethernet Controller', footprint='QFN-64')
+# Define Ethernet Controller and Ports (Example: Realtek RTL8111)
+eth_ctrl = Part('realtek', 'RTL8111', footprint='QFN-64')
 vcc += eth_ctrl['VCC']
 gnd += eth_ctrl['GND']
 cpu['ETH_TX', 'ETH_RX'] += eth_ctrl['TX', 'RX']
 
-eth_port = Part('connector', 'ETHERNET_PORT', value='Ethernet Port', footprint='RJ45')
+eth_port = Part('connector', 'ETHERNET_PORT', footprint='RJ45')
 eth_vcc += eth_port['VCC']
 gnd += eth_port['GND']
 eth_port['TX+'] += eth_ctrl['TX+']
@@ -91,15 +102,15 @@ eth_port['TX-'] += eth_ctrl['TX-']
 eth_port['RX+'] += eth_ctrl['RX+']
 eth_port['RX-'] += eth_ctrl['RX-']
 
-# Define SATA Controller and Ports
-sata_ctrl = Part('device', 'SATA_CONTROLLER', value='SATA Controller', footprint='QFN-64')
+# Define SATA Controller and Ports (Example: Marvell 88SE9215)
+sata_ctrl = Part('marvell', '88SE9215', footprint='QFN-64')
 vcc += sata_ctrl['VCC']
 gnd += sata_ctrl['GND']
 cpu['SATA_TX', 'SATA_RX'] += sata_ctrl['TX', 'RX']
 
 sata_ports = []
 for i in range(4):
-    sata_port = Part('connector', 'SATA_PORT', value=f'SATA Port {i+1}', footprint='SATA')
+    sata_port = Part('connector', 'SATA_PORT', footprint='SATA')
     sata_vcc += sata_port['VCC']
     gnd += sata_port['GND']
     sata_port['TX+'] += sata_ctrl[f'TX+{i}']
@@ -108,19 +119,19 @@ for i in range(4):
     sata_port['RX-'] += sata_ctrl[f'RX-{i}']
     sata_ports.append(sata_port)
 
-# Define clock generator
-clock_gen = Part('device', 'CLOCK_GEN', value='Clock Generator', footprint='QFN-32')
+# Define clock generator (Example: IDT 5V9885)
+clock_gen = Part('idt', '5V9885', footprint='QFN-32')
 vcc += clock_gen['VCC']
 gnd += clock_gen['GND']
 clock_gen['CLK_OUT'] += cpu['CLK']
 
 # Define Voltage Regulator Modules (VRMs) for power integrity
-vrm_cpu = Part('power', 'VRM', value='VRM for CPU', footprint='DPAK')
-vrm_ram = Part('power', 'VRM', value='VRM for RAM', footprint='DPAK')
-vrm_pcie = Part('power', 'VRM', value='VRM for PCIe', footprint='DPAK')
-vrm_usb = Part('power', 'VRM', value='VRM for USB', footprint='DPAK')
-vrm_eth = Part('power', 'VRM', value='VRM for Ethernet', footprint='DPAK')
-vrm_sata = Part('power', 'VRM', value='VRM for SATA', footprint='DPAK')
+vrm_cpu = Part('ti', 'TPS7A4501', footprint='DPAK')
+vrm_ram = Part('ti', 'TPS7A4501', footprint='DPAK')
+vrm_pcie = Part('ti', 'TPS7A4501', footprint='DPAK')
+vrm_usb = Part('ti', 'TPS7A4501', footprint='DPAK')
+vrm_eth = Part('ti', 'TPS7A4501', footprint='DPAK')
+vrm_sata = Part('ti', 'TPS7A4501', footprint='DPAK')
 
 vrm_cpu['IN'] += atx_power['12V']
 vrm_cpu['OUT'] += cpu['VCC']
@@ -141,7 +152,7 @@ vrm_sata['IN'] += atx_power['12V']
 vrm_sata['OUT'] += sata_vcc
 
 # Connect power and ground
-for net in [vcc, gnd, pcie_vcc, pcie_gnd, usb_vcc, eth_vcc, sata_vcc]:
+for net in [vcc, gnd, pcie_vcc, pcie_gnd, usb_vcc, eth_vcc, sata_vcc, fpga_vcc, fpga_gnd]:
     net.drive = POWER
 
 # Define decoupling capacitors for power stability
@@ -162,6 +173,7 @@ add_decoupling_caps(usb_ctrl, 'VCC')
 add_decoupling_caps(eth_ctrl, 'VCC')
 add_decoupling_caps(sata_ctrl, 'VCC')
 add_decoupling_caps(clock_gen, 'VCC')
+add_decoupling_caps(fpga, 'VCC')
 
 # Generate the netlist
 generate_netlist()
